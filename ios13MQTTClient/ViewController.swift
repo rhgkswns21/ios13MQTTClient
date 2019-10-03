d97d8b9d4391b4bedeb1a053009f00f940a2145d
rhgkswns21 <rhgkswns@naver.com>
2019-10-03 18:43:19 +0900

    Initial Commit

---
+++ ios13MQTTClient/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios13MQTTClient
+//
+//  Created by Hanjun Ko on 03/10/2019.
+//  Copyright © 2019 Hanjun Ko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
